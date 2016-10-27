46d582cbed40475c240762c9fe58fe3493c11827
Chris Woerz <erendrake@gmail.com>
2016-10-27 14:27:51 -0600

    return the max status code from running tests

--- dotnet-test.ps1
+++ dotnet-test.ps1
@@ -1,11 +1,16 @@
+$exitCode = 0;
+
 if(Test-Path -Path 'application/test/') {
   Get-ChildItem application/test/ -Recurse -include project.json | ForEach {
     $parent = Split-Path (Split-Path -Path $_.Fullname -Parent) -Leaf;
     $testFile = "TEST-RESULTS-$parent.xml";
     dotnet test $_.Fullname -xml $testFile;
+    $exitCode = [System.Math]::Max($lastExitCode, $exitCode);
   }
 }
 else
 {
   Write-Warning "No test file found.  Skipping tests."
 }
+
+exit $exitCode;
